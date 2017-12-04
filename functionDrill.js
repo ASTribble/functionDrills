c05f57a2dc0663e07cd19c33f2f4c5fab746f897
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-04 12:58:22 -0800

    Add error checks for whoAmI

--- functionDrill.js
+++ functionDrill.js
@@ -13,13 +13,33 @@ function yearOfBirth(age){
 }
 
 function whoAmI(name, age){ 
-    const birthyear = yearOfBirth(age);
-    console.log(`Hi my name is ${name} and I'm ${age} years old.`);
-    if (birthyear !== undefined){
-    console.log(`I was born in ${birthyear}.`);
-    } else {
-        console.log(`And I'm lying about my age ;) `);
+    // Make sure this isn't blank or "undefined"
+    let hasErrors = false;
+    
+    if (typeof name === 'undefined' || typeof age === 'undefined'){
+        console.error('Arguments not valid.');        
+        hasErrors = true;
+    }
+    
+    if (typeof name !== 'string'){
+        console.error('Invalid type for name.');
+        hasErrors = true;}
+
+   if (typeof age !== 'number'){
+        console.error('Invalid type for age.');
+        hasErrors = true;
+    }
+
+    if (!hasErrors) {
+        const birthyear = yearOfBirth(age);
+        console.log(`Hi my name is ${name} and I'm ${age} years old.`);
+        if (birthyear !== undefined){
+        console.log(`I was born in ${birthyear}.`);
+        } else {
+            console.log(`And I'm lying about my age ;) `);
+        }
     }
 }
 
-whoAmI("Mr. Magoo", -65);
+// whoAmI("Mr. Magoo", -65);
+whoAmI("Agent 007",29);
