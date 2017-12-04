1313afddae406474cfef4ef36bfb9ff6dc6db10c
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-04 12:30:34 -0800

    Added try/catch to yearOfBirth function

--- functionDrill.js
+++ functionDrill.js
@@ -1,13 +1,25 @@
 
 
 function yearOfBirth(age){
-    return 2017-age;
+    try {
+        if (age < 0){
+        throw new Error("Age can not be negative.");    
+        } else {
+            return 2017-age;
+        }
+    } catch (error) {
+        console.error(error.message);
+    }            
 }
 
 function whoAmI(name, age){ 
     const birthyear = yearOfBirth(age);
     console.log(`Hi my name is ${name} and I'm ${age} years old.`);
+    if (birthyear !== undefined){
     console.log(`I was born in ${birthyear}.`);
+    } else {
+        console.log(`And I'm lying about my age ;) `);
+    }
 }
 
-whoAmI("Mr. Magoo", 65);
+whoAmI("Mr. Magoo", -65);
